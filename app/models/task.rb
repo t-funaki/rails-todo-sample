28fd267596d97b2213e001f45583f704da54e5b5
t-funaki <funatomo01mail@gmail.com>
2019-05-06 13:55:17 +0900

    バリデーションの実装

--- app/models/task.rb
+++ app/models/task.rb
@@ -1,2 +1,17 @@
 class Task < ApplicationRecord
+  validates :name, presence: true
+  validates :name, length: {maximum: 30}
+  validate :validate_namoe_not_including_conma
+
+  before_validation :set_nameless_name
+
+  private
+
+  def validate_namoe_not_including_conma
+      errors.add(:name, 'にカンマを含めることはできません') if name&.include?(',')
+  end
+
+  def set_nameless_name
+    self.name = '名前なし' if name.blank?
+  end
 end
